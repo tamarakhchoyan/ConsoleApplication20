c0abdc811f11d72f9ad7a45650861d375cf38f99
User <User@DESKTOP-9TGTEUB>
2023-11-28 11:11:10 +0400

    Add project files.

---
+++ ConsoleApplication20/ConsoleApplication20.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+	int a = 4;
+	int b = 5;
+	double c = sqrt(a * a + b * b);
+	int s = a * b / 2;
+	std::cout << c << std::endl;
+	std::cout << s;
+	return 0;
+
+}
